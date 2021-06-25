8a26e92a311057faf2a06f40777aefc526f4f380
AhmedAshraf <ahmed.ashref.1197@gmail.com>
2021-06-25 14:58:06 +0200

    up01

--- server.js
+++ server.js
@@ -2,25 +2,16 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config
 }
+//db set up 
+
 const Mongo = require('mongoose');
 const MongoClient = require("mongodb").MongoClient
 const uri = "mongodb+srv://admin:01159680878@cluster0.vjlte.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const clint = new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 connection()
-async function connection() {
-    try {
-        await clint.connect();
-        const db = clint.db()
-        const users = await db.collection("users")
-        console.log("conected to mongos")
-        return users
-    } catch (e) {
-        console.log(e)
-    }
-}
-//Mongo.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(console.log("conected to mango"))
 
-// Require Express to run server and routes
+// Require 
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -32,17 +23,17 @@ const adminRouter = require("./routs/admin")
 const userRouter = require("./routs/user")
 const mongo = require('mongoose')
 const Userdb = require("./dataBase/userDb")
-
-
 const bcrypt = require("bcryptjs");
 const { json } = require('body-parser');
 
 //creat app
-
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
+app.use(cors());
+const port = 3030;
+const server = app.listen(process.env.PORT || port, listening);
+
 //creating main routers 
 
 app.use("/", indexRouter)
@@ -61,39 +52,63 @@ app.use(express.static(__dirname + '/views'));
 
 app.use("/views", express.static("views"))
 
-app.use(cors());
 
-//setup server
-const port = 3030;
+//get routs
+
 app.get('/', (req, res) => {
         res.render('../Views/logIN/login')
     })
-    //routs post
     
-    app.post('/signIN', async (req, res) => {
+    
+//post routs
+    
+app.post('/signIN', async (req, res) => {
         const db = clint.db()
         const users = await db.collection("userschemas")
-        //console.log(users)
+        const admins = await db.collection("admins")
+        
     let userName = req.body.user_in
     let userBass =req.body.pass_in
     const user =  await users.findOne({stuName : `${userName}`})
-    console.log(user)
+    const admin =  await admins.findOne({stuName : `${userName}`})
+    try{
+        if(admin === null){
+        if(await bcrypt.compare(userBass,user.password))
+        {
+            const token =jwt.sign({
+                id: user._id,
+                username: user.stuName
+            },JWT_SECRET)
+            res.redirect("/user/")
+        }
+        }else if(user === null)
+        {
+        if(await bcrypt.compare(userBass,admin.password))
+        {
+            const token =jwt.sign({
+                id: admin._id,
+                username: admin.adminName
+            },JWT_SECRET)
+            res.redirect("/admin/")
+        }
+    }else{ 
+        res.json("user/pasoword not valed ")
 
-   
-    if (await bcrypt.compare(userBass,user.password)){
-        const token =jwt.sign({
-            id: user._id,
-            username: user.stuName
-        },JWT_SECRET)
-        res.redirect("/user/")
     }
-  
-    });
+    }catch(error){
+         console.error()
+    }
+})
     
 
 app.post("/sginUp", async(req, res) => {
     const db = clint.db()
     const users = await db.collection("userschemas")   
+    const admins = await db.collection("admins")
+    const adminCodeV = req.body.adminCode
+    const admin = await admins.findOne({adminCode : `${adminCodeV}`})     
+
+    if(admin !== null){
     const hashPass = await bcrypt.hash(req.body.password ,10)
     const user = {
         stuName : req.body.stuName,
@@ -110,10 +125,24 @@ app.post("/sginUp", async(req, res) => {
     } catch (error) {
         console.log(error)
         return res.json({status:'error'})
-    }
+    }}
 })
 
-const server = app.listen(process.env.PORT || port, listening);
+//functions 
+
+async function connection() {
+    try {
+        await clint.connect();
+        const db = clint.db()
+        const users = await db.collection("users")
+        console.log("conected to mongos")
+        return users
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+//setup server
 
 function listening() {
     console.log(`server running on http://localhost:${port}`)
